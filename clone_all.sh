2cb8c9d05220f99b094e8d235070a5c5e69382db
angelmoya <amoyapardo@gmail.com>
2022-12-18 12:52:58 +0100

    ADD submodules

---
+++ clone_all.sh
@@ -0,0 +1,23 @@
+git submodule add -b 16.0 https://github.com/oca/account-financial-reporting.git oca/account-financial-reporting
+git submodule add -b 16.0 https://github.com/oca/account-financial-tools.git oca/account-financial-tools
+# git submodule add -b 16.0 https://github.com/oca/account-invoice-reporting.git oca/account-invoice-reporting
+git submodule add -b 16.0 https://github.com/oca/account-invoicing.git oca/account-invoicing
+git submodule add -b 16.0 https://github.com/oca/account-payment.git oca/account-payment
+git submodule add -b 16.0 https://github.com/oca/bank-payment.git oca/bank-payment
+# git submodule add -b 16.0 https://github.com/oca/bank-statement-import.git oca/bank-statement-import
+git submodule add -b 16.0 https://github.com/oca/community-data-files.git oca/community-data-files
+# git submodule add -b 16.0 https://github.com/oca/credit-control.git oca/credit-control
+git submodule add -b 16.0 https://github.com/oca/l10n-spain.git oca/l10n-spain
+git submodule add -b 16.0 https://github.com/oca/mis-builder.git oca/mis-builder
+git submodule add -b 16.0 https://github.com/oca/partner-contact.git oca/partner-contact
+git submodule add -b 16.0 https://github.com/oca/product-attribute.git oca/product-attribute
+git submodule add -b 16.0 https://github.com/oca/project.git oca/project
+git submodule add -b 16.0 https://github.com/oca/purchase-workflow.git oca/purchase-workflow
+git submodule add -b 16.0 https://github.com/oca/queue.git oca/queue
+git submodule add -b 16.0 https://github.com/oca/reporting-engine.git oca/reporting-engine
+git submodule add -b 16.0 https://github.com/oca/sale-workflow.git oca/sale-workflow
+# git submodule add -b 16.0 https://github.com/oca/sale-reporting.git oca/sale-reporting
+git submodule add -b 16.0 https://github.com/oca/server-tools.git oca/server-tools
+git submodule add -b 16.0 https://github.com/oca/server-ux.git oca/server-ux
+# git submodule add -b 16.0 https://github.com/oca/social.git oca/social
+git submodule add -b 16.0 https://github.com/oca/web.git oca/web
